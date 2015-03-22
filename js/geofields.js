9fc9f8a69a1104c9789a00adec937d56c343884b
Роман Кужелев <Kuzhelov@Kuzhelov.local>
2015-03-22 21:10:42 +0300

    Angular support has been interpolated on modal windows for services

--- js/geofields.js
+++ js/geofields.js
@@ -3,37 +3,110 @@
 		.factory('services', function servicesFactory() {
 
 			var commonServiceDescription = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime   quam architecto quo inventore harum ex magni, dicta impedit.";
+			var commonMethodDescription = "Vivamus a dictum nisi. Fusce sed dignissim quam, vel ultricies augue. Vivamus tempor, eros non posuere convallis, odio felis accumsan nunc, in commodo est arcu vitae magna";
 
 			return [
 				{
 					id: "oil",
 					name: "Oil",
 					icon: "flask",
-					description: commonServiceDescription
+					brief: commonServiceDescription,
+					picture: "img/services/oil.jpg",
+					description: "Geofields, offers its customers of the oil industry, tools that enable them to improve the image of its exploration prospects, conduct regional surveys at reasonable cost and decrease the risk in the prospects located in areas of complex tectonics.",
+					methods: [
+						{
+							name: "Magnetotelluric Exploration",
+							description: commonMethodDescription
+						},
+						{
+							name: "3D seismic data processing",
+							description: commonMethodDescription
+						},
+						{
+							name: "Seismic Interpretation",
+							description: commonMethodDescription
+						}
+					]
 				},
 				{
 					id: "mining",
 					name: "Mining",
 					icon: "diamond",
-					description: commonServiceDescription
+					brief: commonServiceDescription,
+					picture: "img/services/mining.jpg",
+					description: "Geofields supports its clients in achieving their exploratory objectives, Geofields has reliable geophysical methods of exploration, experience in mining and oil industries, producing high quality products within the according timeframe and at competitive prices.",
+					methods: [
+						{
+							name: "Electrical methods",
+							description: commonMethodDescription
+						},
+						{
+							name: "Magnetic and Gravity Surveys",
+							description: commonMethodDescription
+						},
+						{
+							name: "Magnetotelluric studies",
+							description: commonMethodDescription
+						}
+					]
 				},
 				{
 					id: "coal",
 					name: "Coal",
 					icon: "cube",
-					description: commonServiceDescription
+					brief: commonServiceDescription,
+					picture: "img/services/coal.jpg",
+					description: "Geofields, thinking about its customers of the coal industry, has designed some solutions ranging from conventional geophysical methods to more sophisticated methods.",
+					methods: [
+						{
+							name: "Electrical Tomography",
+							description: commonMethodDescription
+						},
+						{
+							name: "Vertical Electrical Sounding",
+							description: commonMethodDescription
+						}
+					]
 				},
 				{
 					id: "groundwater",
 					name: "Geothermal and Groundwater",
 					icon: "fire",
-					description: commonServiceDescription
+					brief: commonServiceDescription,
+					picture: "img/services/groundwater.png",
+					description: "Growing concerns about climate change seen in clean alternative energy is an option to reduce the emission of polluting gases into the atmosphere. The power generation from geothermal sources is a practical alternative to being friendly to the environment because it does not affect the ecosystem by not having to inundate large areas for energy generation. Colombia has great potential for research in this area. Geofields with Magnetotelluric method provides an important tool for locating geothermal sources capable of moving with the aid of steam turbines and generate clean energy.",
+					methods: [
+						{
+							name: "Deep Magnetotelluric Sounding",
+							description: commonMethodDescription
+						},
+						{
+							name: "Audio Magnetotelluric Sounding",
+							description: commonMethodDescription
+						}
+					]
 				},
 				{
 					id: "civil",
 					name: "Civil Works",
 					icon: "male",
-					description: commonServiceDescription
+					brief: commonServiceDescription,
+					picture: "img/services/civil.jpg",
+					description: "Geofields provides high quality specialized geophysical services for engineering studies to support engineering in the construction of hydroelectric dams, bridges, tunnels and major works. Geofields's commitment is to advise and accompany their clients in selecting the most appropriate to their exploration needs method. That's why we offer to our customers a range of geophysical methods that will help them gain a better understanding about the properties of the subsoil as a basis for its engineering projects.",
+					methods: [
+						{
+							name: "Seismic Refraction",
+							description: commonMethodDescription
+						},
+						{
+							name: "Electrical Prospecting methods",
+							description: commonMethodDescription
+						},
+						{
+							name: "Magnetotelluric studies",
+							description: commonMethodDescription
+						}
+					]
 				}
 			];
 		})
@@ -64,12 +137,12 @@
 			const servicesPerRow = 3;
 			var serviceRows = [];
 
-			var serviceItems = services;
+			var serviceItems = services.slice();
 
 			while (serviceItems.length) {
 				serviceRows.push(
 					{
-						'services': services.splice(0, servicesPerRow)
+						'services': serviceItems.splice(0, servicesPerRow)
 					});
 			}
 
@@ -82,5 +155,8 @@
 		.controller('ProjectsController', ['$scope', 'projects', function($scope, projects) {
 
 			$scope.projects = projects;
+		}])
+		.controller('ServiceModalsController', ['$scope', 'services', function($scope, services) {
+			$scope.services = services;
 		}]);
 })();
